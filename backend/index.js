8c17f2fbc694e5eb197251b83ba27022c1a0cec2
Sami Khan <samikhan7816@gmail.com>
2024-01-19 00:05:26 +0500

    updated

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,20 @@ import colors from "colors";
 import cors from "cors";
 import dotenv from "dotenv";
 import dbConnection from "./config/dbConnection.js";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Get the current filename
+const __filename = fileURLToPath(import.meta.url);
+
+// Get the directory name
+const __dirname = dirname(__filename);
+
 // user Routes
 import userRouter from "./routes/userRoutes.js";
 // blog Routes
 import blogRouter from "./routes/blogRoutes.js";
+import path from 'path';
 // Env config
 dotenv.config();
 
@@ -18,17 +28,23 @@ dbConnection();
 const app = express(); 
 
 // Using middlewares
+
 app.use(cors({credentials:true}));
 app.use(express.json());
 app.use(morgan("dev"));
 // Making routes
+
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/blog", blogRouter);
 app.use((req, res) => {
-    res.status(404).send('Route Not Found');
+    res.status(404).send('Route Not Found Here');
 });
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.get('*', (req, res)=> {
+    res.sendFile(path.join(__dirname,'../frontend/dist/index.html'))
+})
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080; 
 
 app.listen(port, () => {
     console.log(`App is running at http://localhost:${port}`);
