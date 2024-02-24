ca9bd20c5098424c73400bb62ce4799ad62fae4f
Sami Khan <samikhan7816@gmail.com>
2024-02-25 01:22:46 +0500

    updated

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -5,7 +5,7 @@ const dbConnection = async () => {
     try {
         await mongoose.connect(process.env.DB_URL);
         console.log(`Database connected Successfully ${mongoose.connection.host}`.bgBlue)
-        console.log(`Database URL: ${process.env.DB_URL}`);
+        // console.log(`Database URL: ${process.env.DB_URL}`);
 
     } catch (error) {
         console.log(`${error} database connection error`)
