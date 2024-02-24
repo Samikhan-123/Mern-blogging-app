e982ccce94d0a618760d0d12eb8b129897952795
Sami Khan <samikhan7816@gmail.com>
2024-02-24 09:56:49 +0500

    updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,9 @@ import preact from '@preact/preset-vite';
 export default defineConfig({
   server: {
     proxy: {
-      "/api/v1": {
+      "/api": {
         target: "https://mern-blogging-app.vercel.app",
-        // target: "http://localhost:5000",
+        target: "http://localhost:5000",
         changeOrigin: true,
         credentials: true
       }
