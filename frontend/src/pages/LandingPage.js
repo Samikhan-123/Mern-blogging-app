a3c8521bbc8083c7adb931872ffda5f335de77c1
Sami Khan <samikhan7816@gmail.com>
2024-02-25 15:10:17 +0500

    updated

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -91,7 +91,7 @@ const LandingPage = () => {
 
                 {/* <!-- Copyright --> */}
                 <div className="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-                    © 2024 Copyright: BlogFolio
+                    © 2024 Copyright:BlogFolio
                 </div>
             </footer>
 
